b404947160a605acdd37242440344eb7381ddac7
Dave Glover <dglover@microsoft.com>
2021-06-30 16:44:36 +1000

    Update azure_iot_config.py
    refactor only

--- Generator/builders/azure_iot_config.py
+++ Generator/builders/azure_iot_config.py
@@ -18,30 +18,19 @@ class Builder():
 
             for key, value in manifest_updates.items():
                 if type(value) is list:
+
                     manifest_list = manifest.get(key, [])
+
                     if type(manifest_list) is dict:
                         for item in value:
                             for k, v in item.items():
 
                                 manifest_list.update({k: v})
 
-                                # The commented out code updated an element if it existed
-                                # manifest_list_value = manifest_list.get(k)
-                                # if manifest_list_value is None:
-                                #     manifest_list.update({k:v})
-                                # else:
-                                #     if type(manifest_list_value) is list:
-                                #         new_list = manifest_list_value + v
-                                #         new_list = list(dict.fromkeys(new_list))
-                                #         manifest_list.update({k:new_list})
-                                #     elif type(manifest_list_value) is str:
-                                #         manifest_list.update({k:v})
                         new_manifest.update({key: manifest_list})
+                        
                     elif type(manifest_list) is list:
-                        # The commented out code updated an element if it existed
-                        # new_list = value + manifest_list
-                        # new_list = list(dict.fromkeys(new_list))
-                        # new_manifest.update({key:new_list})
+
                         new_manifest.update({key: value})
 
         return new_manifest
